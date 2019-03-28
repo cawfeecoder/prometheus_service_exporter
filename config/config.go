ed9e6ed3a5d1eb11520eeb321ff11c14791944b3
Nicholas Frush <nick@frushco.de>
2019-03-28 00:35:49 -0400

    Revise regex

--- config/config.go
+++ config/config.go
@@ -31,13 +31,7 @@ func LoadConfig(path string) (config Config, err error){
     	return
 	}
     for _, v := range viper.GetStringSlice("service_whitelist"){
-    	var r *regexp.Regexp
-    	var err error
-    	if config.Supervisor == "systemd" {
-    		r, err = regexp.Compile(v + ".service")
-		} else {
-			r, err = regexp.Compile("\b" + v + "\b")
-		}
+    	r, err := regexp.Compile(v)
     	if err != nil {
     		err = errors.New("one or more whitelist values are not a valid string or regex")
     		break
